69a6f6ab24361ebfe60acd087ef31ac4ee02b8ac
Vamsi TP <vamsitp@microsoft.com>
2019-10-10 14:27:13 +0530

    Using Flurl.Http instead of HttpClient

--- PromoTagz/Program.cs
+++ PromoTagz/Program.cs
@@ -21,29 +21,44 @@
 
     public class Program
     {
-        private const string RelationsQueryPath = "wit/wiql?api-version=" + ApiVersion; //"queries/Shared Queries/EFUs";
-        private const string WorkItemsQueryPath = "wit/workitems?ids={0}&api-version=" + ApiVersion; //"queries/Shared Queries/EFUs";
-
-        private static string Account => ConfigurationManager.AppSettings[nameof(Account)];
-        private static string Project => ConfigurationManager.AppSettings["Project"];
-        private static string Token => ConfigurationManager.AppSettings["PersonalAccessToken"];
-
-        private static string BaseUrl => $"https://dev.azure.com/{Account}/{Project}/_apis/wit";
-
-        private static string WorkItemUpdateUrl => $"{BaseUrl}/workItems/{{0}}?api-version={ApiVersion}";
-
-        private static char[] TagsDelimiter = new char[] { ',', ';' };
-
-        private static List<string> TagsToPromote => ConfigurationManager.AppSettings[nameof(TagsToPromote)].Split(TagsDelimiter, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToList();
-
-        private static readonly string WorkItemsQuery = ConfigurationManager.AppSettings["WorkItemsQuery"];
-
         private const string WorkItemsJson = "./WorkItems.json";
         private const string Dot = ". ";
         private const string ApiVersion = "4.1-preview";
-        private static List<EFU> efus = null;
-        private static List<WorkItem> workitems = null;
+        private const string JsonPatchMediaType = "application/json-patch+json";
+        private const string JsonMediaType = "application/json";
+        private const string AuthHeader = "Authorization";
+        private const string BasicAuth = "Basic ";
+        private const string SystemTags = "/fields/System.Tags";
+        private const string AddOperation = "add";
+        private const string UserStory = "User Story";
+        private const string Pbi = "Product Backlog Item";
+        private const string Feature = "Feature";
 
+        private static string Account => ConfigurationManager.AppSettings[nameof(Account)];
+
+        private static string Project => ConfigurationManager.AppSettings["Project"];
+
+        private static string Token => ConfigurationManager.AppSettings["PersonalAccessToken"];
+
+        private static string Pat => Convert.ToBase64String(Encoding.ASCII.GetBytes($":{Token ?? string.Empty}"));
+
+        private static string BaseUrl => $"https://dev.azure.com/{Account}/{Project}/_apis/wit";
+
+        private static string RelationsQueryPath = $"{BaseUrl}/wiql?api-version={ApiVersion}"; //"queries/Shared Queries/EFUs";
+
+        private static string WorkItemsQueryPath = $"{BaseUrl}/workitems?ids={{0}}&api-version={ApiVersion}"; //"queries/Shared Queries/EFUs";
+
+        private static string WorkItemUpdateUrl => $"{BaseUrl}/workItems/{{0}}?api-version={ApiVersion}";
+
+        private static char[] TagsDelimiters = new char[] { ',', ';' };
+
+        private static List<string> TagsToPromote => ConfigurationManager.AppSettings[nameof(TagsToPromote)].Split(TagsDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToList();
+
+        private static readonly string WorkItemsQuery = ConfigurationManager.AppSettings["WorkItemsQuery"];
+
+        private static List<EFU> efus = null;
+
+        private static List<WorkItem> workitems = null;
 
         [STAThread]
         public static void Main(string[] args)
@@ -117,7 +132,7 @@
             if (efus == null)
             {
                 efus = new List<EFU>();
-                var wis = await GetData<WiqlRelationList>(RelationsQueryPath, Project, "{\"query\": \"" + string.Format(WorkItemsQuery, Project) + "\"}"); // AND [Source].[System.AreaPath] UNDER '{1}'
+                var wis = await ProcessRequest<WiqlRelationList>(RelationsQueryPath, "{\"query\": \"" + string.Format(WorkItemsQuery, Project) + "\"}"); // AND [Source].[System.AreaPath] UNDER '{1}'
                 ColorConsole.WriteLine($"Work-item relations fetched: {wis.workItemRelations.Length}");
                 var rootItems = wis.workItemRelations.Where(x => x.source == null).ToList();
                 await IterateWorkItems(rootItems, null, wis).ConfigureAwait(false);
@@ -138,7 +153,7 @@
                         Id = wi.id,
                         Title = wi.fields.SystemTitle,
                         Workitemtype = wi.fields.SystemWorkItemType,
-                        Tags = wi.fields.SystemTags?.Split(TagsDelimiter, StringSplitOptions.RemoveEmptyEntries)?.Select(x => x.Trim())?.ToList(),
+                        Tags = wi.fields.SystemTags?.Split(TagsDelimiters, StringSplitOptions.RemoveEmptyEntries)?.Select(x => x.Trim())?.ToList(),
                         Parent = parent?.Id
                     };
 
@@ -166,7 +181,7 @@
                     var ids = builder.ToString().TrimEnd(',');
                     if (!string.IsNullOrWhiteSpace(ids))
                     {
-                        var workItems = await GetData<WorkItems>(string.Format(WorkItemsQueryPath, ids), Project, string.Empty);
+                        var workItems = await ProcessRequest<WorkItems>(string.Format(WorkItemsQueryPath, ids));
                         if (workItems != null)
                         {
                             result.AddRange(workItems.Items);
@@ -180,10 +195,10 @@
 
         private static async Task SaveWorkItemsAsync()
         {
-            var grouping = efus.Where(x => x.Workitemtype.Equals("User Story") || x.Workitemtype.Equals("Product Backlog Item")).GroupBy(x => x.Parent); // .OrderByDescending(x => x.Id)
+            var grouping = efus.Where(x => x.Workitemtype.Equals(UserStory) || x.Workitemtype.Equals(Pbi)).GroupBy(x => x.Parent); // .OrderByDescending(x => x.Id)
             await SaveWorkItemsAsync(grouping).ConfigureAwait(false);
 
-            grouping = efus.Where(x => x.Workitemtype.Equals("Feature")).GroupBy(x => x.Parent);
+            grouping = efus.Where(x => x.Workitemtype.Equals(Feature)).GroupBy(x => x.Parent);
             await SaveWorkItemsAsync(grouping).ConfigureAwait(false);
         }
 
@@ -257,61 +272,43 @@
             return update;
         }
 
-        public static async Task<bool> SaveWorkItemsAsync(EFU workItem)
+        public static async Task SaveWorkItemsAsync(EFU workItem)
         {
             var tags = string.Join(';', workItem.Tags);
-            var pat = Convert.ToBase64String(Encoding.ASCII.GetBytes($":{Token}"));
             var ops = new[]
                 {
-                    new Op { op = "add", path = "/fields/System.Tags", value = tags }
+                    new Op { op = AddOperation, path = SystemTags, value = tags }
                 }.ToList();
 
-            var content = new CapturedStringContent(ops?.ToJson(), Encoding.UTF8, "application/json-patch+json");
-            try
-            {
-                var result = await string.Format(CultureInfo.InvariantCulture, WorkItemUpdateUrl, workItem.Id)
-                            .WithHeader("Authorization", "Basic " + pat)
-                            .PatchAsync(content)
-                            .ConfigureAwait(false);
-
-                ColorConsole.WriteLine($"Updated Tags for {workItem.Id}: {tags}".White().OnDarkGreen());
-                var success = result.StatusCode == System.Net.HttpStatusCode.OK;
-                return success;
-            }
-            catch (Exception ex)
-            {
-                var err = await ex.ToFullStringAsync().ConfigureAwait(false);
-                WriteError(err);
-                return false;
-            }
+            var result = await ProcessRequest<dynamic>(string.Format(CultureInfo.InvariantCulture, WorkItemUpdateUrl, workItem.Id), ops?.ToJson(), true).ConfigureAwait(false);
+            ColorConsole.WriteLine($"Updated Tags for {workItem.Id}: {tags}".White().OnDarkGreen());
         }
 
-        private static async Task<T> GetData<T>(string path, string project, string postData)
+        private static async Task<T> ProcessRequest<T>(string path, string content = null, bool patch = false)
         {
-            // https://www.visualstudio.com/en-us/docs/integrate/api/wit/samples
-            using (var client = new HttpClient())
+            try
             {
-                var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($":{Token}"));
-                client.BaseAddress = new Uri(Account.IndexOf(".com", StringComparison.OrdinalIgnoreCase) > 0 ? Account : $"https://{Account}.visualstudio.com");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
-                if (!path.StartsWith(Account, StringComparison.OrdinalIgnoreCase))
-                {
-                    path = $"{project}/_apis/{path}";
-                }
-
-                Trace.TraceInformation($"BaseAddress: {Account} | Path: {path} | Content: {postData}");
+                // https://www.visualstudio.com/en-us/docs/integrate/api/wit/samples
+                Trace.TraceInformation($"BaseAddress: {Account} | Path: {path} | Content: {content}");
                 HttpResponseMessage queryHttpResponseMessage;
+                var request = path.WithHeader(AuthHeader, BasicAuth + Pat);
 
-                if (string.IsNullOrWhiteSpace(postData))
+                if (string.IsNullOrWhiteSpace(content))
                 {
-                    queryHttpResponseMessage = await client.GetAsync(path);
+                    queryHttpResponseMessage = await request.GetAsync().ConfigureAwait(false);
                 }
                 else
                 {
-                    var content = new StringContent(postData, Encoding.UTF8, "application/json");
-                    queryHttpResponseMessage = await client.PostAsync(path, content);
+                    if (patch)
+                    {
+                        var stringContent = new CapturedStringContent(content, Encoding.UTF8, JsonPatchMediaType);
+                        queryHttpResponseMessage = await request.PatchAsync(stringContent).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        var stringContent = new StringContent(content, Encoding.UTF8, JsonMediaType);
+                        queryHttpResponseMessage = await request.PostAsync(stringContent).ConfigureAwait(false);
+                    }
                 }
 
                 if (queryHttpResponseMessage.IsSuccessStatusCode)
@@ -323,6 +320,12 @@
                 {
                     throw new Exception($"{queryHttpResponseMessage.ReasonPhrase}");
                 }
+            }
+            catch (Exception ex)
+            {
+                var err = await ex.ToFullStringAsync().ConfigureAwait(false);
+                WriteError(err);
+                return default(T);
             }
         }
 
