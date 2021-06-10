9d98bec86d441dafcea12a903c47f05d8a25abfe
Vamsi TP <vamsitp@microsoft.com>
2021-06-10 08:46:07 +0530

    PromoDesc

---
+++ PromoDesc/Program.cs
@@ -0,0 +1,344 @@
+﻿namespace PromoDesc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Configuration;
+    using System.Data;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ClosedXML.Excel;
+    using ColoredConsole;
+
+    using Flurl.Http;
+    using Flurl.Http.Content;
+
+    using Newtonsoft.Json;
+
+    public class Program
+    {
+        private const string WorkItemsJson = "./WorkItems.json";
+        private const string Dot = ". ";
+        private const string ApiVersion = "5.1";
+        private const string JsonPatchMediaType = "application/json-patch+json";
+        private const string JsonMediaType = "application/json";
+        private const string AuthHeader = "Authorization";
+        private const string BasicAuth = "Basic ";
+        private const string SystemDescription = "/fields/System.Description";
+        private const string AddOperation = "add";
+        private const string UserStory = "User Story";
+        private const string Pbi = "Product Backlog Item";
+        private const string Feature = "Feature";
+        public static char[] TagsDelimiters = new char[] { ' ', ',', ';' };
+
+        private static string Account => ConfigurationManager.AppSettings[nameof(Account)];
+
+        private static string Project => ConfigurationManager.AppSettings[nameof(Project)];
+
+        private static string PersonalAccessToken => ConfigurationManager.AppSettings[nameof(PersonalAccessToken)];
+
+        private static string Pat => Convert.ToBase64String(Encoding.ASCII.GetBytes($":{PersonalAccessToken ?? string.Empty}"));
+
+        private static string BaseUrl => $"https://dev.azure.com/{Account}/{Project}/_apis/wit";
+
+        private static string RelationsQueryPath = $"{BaseUrl}/wiql?api-version={ApiVersion}"; //"queries/Shared Queries/EFUs";
+
+        private static string WorkItemsQueryPath = $"{BaseUrl}/workitems?ids={{0}}&api-version={ApiVersion}"; //"queries/Shared Queries/EFUs";
+
+        private static string WorkItemUpdateUrl => $"{BaseUrl}/workItems/{{0}}?api-version={ApiVersion}";
+
+        private static string WorkItemUpdatesUrl => $"{BaseUrl}/workItems/{{0}}/updates?api-version={ApiVersion}";
+
+        private static readonly string WorkItemsQuery = ConfigurationManager.AppSettings["WorkItemsQuery"];
+
+        private static bool ReportOnly => ConfigurationManager.AppSettings.AllKeys.Contains(nameof(ReportOnly)) && bool.Parse(ConfigurationManager.AppSettings[nameof(ReportOnly)]);
+
+        private static List<EFU> efus = null;
+
+        private static List<WorkItem> workitems = null;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Execute().Wait();
+            ColorConsole.WriteLine($"\nDone! Press 'O' to open the file or any other key to exit...".White().OnGreen());
+            var key = Console.ReadKey();
+        }
+
+        public static async Task Execute()
+        {
+            try
+            {
+                CheckSettings();
+                await ProcessWorkItems();
+            }
+            catch (Exception ex)
+            {
+                WriteError(ex.Message);
+            }
+        }
+
+        private static void CheckSettings(bool reset = false)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var section = config.Sections.OfType<AppSettingsSection>().FirstOrDefault();
+            var settings = section.Settings;
+            var save = false;
+
+            if (reset)
+            {
+                ConfigurationManager.AppSettings.Set(nameof(Account), string.Empty);
+                ConfigurationManager.AppSettings.Set(nameof(Project), string.Empty);
+                ConfigurationManager.AppSettings.Set(nameof(PersonalAccessToken), string.Empty);
+            }
+
+            if (string.IsNullOrWhiteSpace(Account) || string.IsNullOrWhiteSpace(Project) || string.IsNullOrWhiteSpace(PersonalAccessToken))
+            {
+                ColorConsole.WriteLine("\n Please provide Azure DevOps details in the format (without braces): <Account> <Project> <PersonalAccessToken>".Black().OnCyan());
+                var details = Console.ReadLine().Split(' ')?.ToList();
+
+                for (var i = 0; i < 3; i++) // Only 4 values required
+                {
+                    var key = settings.AllKeys[i];
+                    settings[key].Value = details[i];
+                }
+
+                save = true;
+            }
+
+            if (save)
+            {
+                config.Save(ConfigurationSaveMode.Minimal);
+                ConfigurationManager.RefreshSection(section.SectionInformation.Name);
+            }
+        }
+
+        private static async Task ProcessWorkItems()
+        {
+            await ProcessWorkItemDescAsync().ConfigureAwait(false);
+        }
+
+        private static void WriteError(string error)
+        {
+            ColorConsole.WriteLine($"Error: {error}".White().OnRed());
+        }
+
+        private static async Task ProcessWorkItemDescAsync()
+        {
+            if (efus == null)
+            {
+                efus = new List<EFU>();
+                var wis = await ProcessRequest<WiqlRelationList>(RelationsQueryPath, "{\"query\": \"" + string.Format(WorkItemsQuery, Project) + "\"}"); // AND [Source].[System.AreaPath] UNDER '{1}'
+                if (wis != null)
+                {
+                    ColorConsole.WriteLine($"Work-item relations fetched: {wis.workItemRelations.Length}");
+                    var rootItems = wis.workItemRelations.Where(x => x.source == null).ToList();
+                    await IterateWorkItems(rootItems, null, wis).ConfigureAwait(false);
+                    await PromoteWorkItemDescriptionAsync().ConfigureAwait(false);
+                }
+                else
+                {
+                    CheckSettings(true);
+                    await ProcessWorkItemDescAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        private static async Task IterateWorkItems(List<WorkitemRelation> relations, EFU parent, WiqlRelationList wis)
+        {
+            if (relations.Count > 0)
+            {
+                workitems = await GetWorkItems(relations.ToList());
+                foreach (var wi in workitems)
+                {
+                    ColorConsole.WriteLine($" {wi.fields.SystemWorkItemType} ".PadLeft(22) + wi.id.ToString().PadLeft(6) + Dot + wi.fields.SystemTitle + $" [Desc: {wi.fields.SystemDescription}]");
+                    var efu = new EFU
+                    {
+                        Id = wi.id,
+                        Title = wi.fields.SystemTitle,
+                        Description = wi.fields.SystemDescription,
+                        Workitemtype = wi.fields.SystemWorkItemType,
+                        Tags = wi.fields.SystemTags?.Split(TagsDelimiters, StringSplitOptions.RemoveEmptyEntries)?.Select(x => x.Trim())?.ToList(),
+                        Parent = parent?.Id
+                    };
+
+                    efus.Add(efu);
+                    parent?.Children.Add(efu.Id);
+                    await IterateWorkItems(wis.workItemRelations.Where(x => x.source != null && x.source.id.Equals(wi.id)).ToList(), efu, wis);
+                }
+            }
+        }
+
+        private static async Task<List<WorkItem>> GetWorkItems(List<WorkitemRelation> items)
+        {
+            var result = new List<WorkItem>();
+            var splitItems = items.SplitList();
+            if (splitItems?.Any() == true)
+            {
+                foreach (var relations in splitItems)
+                {
+                    var builder = new StringBuilder();
+                    foreach (var item in relations.Select(x => x.target))
+                    {
+                        builder.Append(item.id.ToString()).Append(',');
+                    }
+
+                    var ids = builder.ToString().TrimEnd(',');
+                    if (!string.IsNullOrWhiteSpace(ids))
+                    {
+                        var workItems = await ProcessRequest<WorkItems>(string.Format(WorkItemsQueryPath, ids));
+                        if (workItems != null)
+                        {
+                            result.AddRange(workItems.Items);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static async Task PromoteWorkItemDescriptionAsync()
+        {
+            var grouping = efus.Where(x => x.Workitemtype.Equals(UserStory) || x.Workitemtype.Equals(Pbi)).GroupBy(x => x.Parent); // .OrderByDescending(x => x.Id)
+            await PromoteWorkItemDescriptionAsync(grouping).ConfigureAwait(false);
+
+            grouping = efus.Where(x => x.Workitemtype.Equals(Feature)).GroupBy(x => x.Parent);
+            await PromoteWorkItemDescriptionAsync(grouping).ConfigureAwait(false);
+
+            if (ReportOnly)
+            {
+                ColorConsole.WriteLine(" Skipped saving PBIs as 'ReportOnly' is set to true in config.".Black().OnWhite());
+            }
+        }
+
+        private static async Task PromoteWorkItemDescriptionAsync(IEnumerable<IGrouping<int?, EFU>> grouping)
+        {
+            foreach (var group in grouping)
+            {
+                var parentId = group.Key;
+                if (parentId != null)
+                {
+                    var descs = group.Where(x => x.Description != null).Select(x => x.Title + "<br />" + x.Description)?.Distinct()?.ToList();
+                    var parent = efus.SingleOrDefault(x => x.Id.Equals(parentId));
+
+                    // Add
+                    if (descs != null)
+                    {
+                        var desc = "<br /><b>ACAI:</b><br />" + string.Join("<br />", descs);
+                        await UpdateParentDescription(parent, desc).ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+
+        private static async Task<bool> UpdateParentDescription(EFU parent, string desc)
+        {
+            var update = false;
+            if (parent != null && parent.Description?.Contains(desc) != true)
+            {
+                ColorConsole.WriteLine($" Description '{desc}' promoted to parent '{parent.Id}'".Yellow());
+                if (!ReportOnly)
+                {
+                    await SaveWorkItemsAsync(parent, desc).ConfigureAwait(false);
+                }
+            }
+
+            return update;
+        }
+
+        public static async Task SaveWorkItemsAsync(EFU workItem, string desc)
+        {
+            var ops = new[]
+            {
+                new Op { op = AddOperation, path = SystemDescription, value = workItem.Description + desc }
+            }.ToList();
+
+            var result = await ProcessRequest<dynamic>(string.Format(CultureInfo.InvariantCulture, WorkItemUpdateUrl, workItem.Id), ops?.ToJson(), true).ConfigureAwait(false);
+            ColorConsole.WriteLine($"Updated Description for {workItem.Id}: {desc}".White().OnDarkGreen());
+        }
+
+        private static async Task<T> ProcessRequest<T>(string path, string content = null, bool patch = false)
+        {
+            try
+            {
+                // https://www.visualstudio.com/en-us/docs/integrate/api/wit/samples
+                Trace.TraceInformation($"BaseAddress: {Account} | Path: {path} | Content: {content}");
+                HttpResponseMessage queryHttpResponseMessage;
+                var request = path.WithHeader(AuthHeader, BasicAuth + Pat);
+
+                if (string.IsNullOrWhiteSpace(content))
+                {
+                    queryHttpResponseMessage = await request.GetAsync().ConfigureAwait(false);
+                }
+                else
+                {
+                    if (patch)
+                    {
+                        var stringContent = new CapturedStringContent(content, Encoding.UTF8, JsonPatchMediaType);
+                        queryHttpResponseMessage = await request.PatchAsync(stringContent).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        var stringContent = new StringContent(content, Encoding.UTF8, JsonMediaType);
+                        queryHttpResponseMessage = await request.PostAsync(stringContent).ConfigureAwait(false);
+                    }
+                }
+
+                if (queryHttpResponseMessage.IsSuccessStatusCode)
+                {
+                    var result = await queryHttpResponseMessage.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(result);
+                }
+                else
+                {
+                    throw new Exception($"{queryHttpResponseMessage.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                var err = await ex.ToFullStringAsync().ConfigureAwait(false);
+                WriteError(err);
+                return default(T);
+            }
+        }
+
+        // Credit: https://stackoverflow.com/questions/18100783/how-to-convert-a-list-into-data-table
+        private static DataTable ToDataTable<T>(IList<T> data)
+        {
+            var properties = TypeDescriptor.GetProperties(typeof(T));
+            var table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+            {
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (T item in data)
+            {
+                var row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                {
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                }
+
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        private class Op
+        {
+            public string op { get; set; }
+
+            public string path { get; set; }
+
+            public dynamic value { get; set; }
+        }
+    }
+}
---
+++ PromoDesc/WorkItem.cs
@@ -0,0 +1,166 @@
+﻿namespace PromoDesc
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public class WiqlList
+    {
+        public string queryType { get; set; }
+        public string queryResultType { get; set; }
+        public DateTime asOf { get; set; }
+        public Column[] columns { get; set; }
+        public Sortcolumn[] sortColumns { get; set; }
+        public WiqlWorkitem[] workItems { get; set; }
+    }
+
+    public class WiqlRelationList
+    {
+        public string queryType { get; set; }
+        public string queryResultType { get; set; }
+        public DateTime asOf { get; set; }
+        public Column[] columns { get; set; }
+        public Sortcolumn[] sortColumns { get; set; }
+        public WorkitemRelation[] workItemRelations { get; set; }
+    }
+
+    public class Column
+    {
+        public string referenceName { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Sortcolumn
+    {
+        public Field field { get; set; }
+        public bool descending { get; set; }
+    }
+
+    public class Field
+    {
+        public string referenceName { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class WiqlWorkitem
+    {
+        public int id { get; set; }
+        public string url { get; set; }
+    }
+
+    public class WorkItem
+    {
+        public int id { get; set; }
+        public int rev { get; set; }
+        public Fields fields { get; set; }
+        public Links _links { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Fields
+    {
+        [JsonProperty("System.AreaPath")]
+        public string SystemAreaPath { get; set; }
+
+        [JsonProperty("System.TeamProject")]
+        public string SystemTeamProject { get; set; }
+
+        [JsonProperty("System.IterationPath")]
+        public string SystemIterationPath { get; set; }
+
+        [JsonProperty("System.WorkItemType")]
+        public string SystemWorkItemType { get; set; }
+
+        [JsonProperty("System.State")]
+        public string SystemState { get; set; }
+
+        [JsonProperty("System.Title")]
+        public string SystemTitle { get; set; }
+
+        [JsonProperty("System.Description")]
+        public string SystemDescription { get; set; }
+
+        [JsonProperty("System.Tags")]
+        public string SystemTags { get; set; }
+    }
+
+    public class Links
+    {
+        public Self self { get; set; }
+        public Workitemupdates workItemUpdates { get; set; }
+        public Workitemrevisions workItemRevisions { get; set; }
+        public Workitemhistory workItemHistory { get; set; }
+        public Html html { get; set; }
+        public Workitemtype workItemType { get; set; }
+        public LinkFields fields { get; set; }
+    }
+
+    public class Self
+    {
+        public string href { get; set; }
+    }
+
+    public class Workitemupdates
+    {
+        public string href { get; set; }
+    }
+
+    public class Workitemrevisions
+    {
+        public string href { get; set; }
+    }
+
+    public class Workitemhistory
+    {
+        public string href { get; set; }
+    }
+
+    public class Html
+    {
+        public string href { get; set; }
+    }
+
+    public class Workitemtype
+    {
+        public string href { get; set; }
+    }
+
+    public class LinkFields
+    {
+        public string href { get; set; }
+    }
+
+    public class WorkitemRelation
+    {
+        public WiqlWorkitem target { get; set; }
+        public string rel { get; set; }
+        public WiqlWorkitem source { get; set; }
+    }
+
+    public class EFU
+    {
+        public EFU()
+        {
+            this.Children = new List<int>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Workitemtype { get; set; }
+        public List<int> Children { get; set; }
+        public int? Parent { get; set; }
+        public IList<string> Tags { get; set; }
+    }
+
+    public class WorkItems
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+        [JsonProperty("value")]
+        public WorkItem[] Items { get; set; }
+    }
+}
